2873ee71413bf0b84cc9e7f1140f149fcd1b142f
Simon Wachira <kimaniwachira03@gmail.com>
2026-03-06 05:16:45 +0300

    update: Index tab and iconbutton component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
 import { View } from "react-native";
 
+import IconButton from "@/components/ui/iconButton";
 import { CameraView } from "expo-camera";
 
 export default function HomeScreen() {
   const cameraRef = React.useRef<CameraView>(null);
   return (
     <View style={{ flex: 1 }}>
-      <CameraView ref={cameraRef} style={{ flex: 1 }} />
+      <CameraView ref={cameraRef} style={{ flex: 1 }}>
+        <IconButton
+          androidName="accessibility"
+          iosName="0.circle"
+          onPress={() => {}}
+        />
+      </CameraView>
     </View>
   );
 }
--- components/ui/iconButton.tsx
+++ components/ui/iconButton.tsx
@@ -1,8 +1,51 @@
+import { Ionicons } from "@expo/vector-icons";
+import { SFSymbol, SymbolView } from "expo-symbols";
+import { ComponentProps } from "react";
+import { Pressable, StyleProp, ViewStyle } from "react-native";
+
+const CONTAINER_PADDING = 5;
+const CONTAINER_WIDTH = 34;
+const ICON_SIZE = 25;
+
 interface IconButtonProps {
-  name: string;
-  size?: number;
-  color?: string;
+  iosName: SFSymbol;
+  androidName: ComponentProps<typeof Ionicons>["name"];
+  contaianerStyle?: StyleProp<ViewStyle>;
   onPress?: () => void;
+  width?: number;
+  height?: number;
 }
 
-export default function IconButton() {}
+export default function IconButton({
+  androidName,
+  iosName,
+  contaianerStyle,
+  onPress,
+  width,
+  height,
+}: IconButtonProps) {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[
+        {
+          backgroundColor: "#00000050",
+          borderRadius: (CONTAINER_WIDTH + CONTAINER_PADDING * 2) / 2,
+          width: CONTAINER_WIDTH,
+          padding: CONTAINER_PADDING,
+        },
+        contaianerStyle,
+      ]}
+    >
+      <SymbolView
+        name={iosName}
+        size={ICON_SIZE}
+        style={width && height ? { width, height } : {}}
+        tintColor={"white"}
+        fallback={
+          <Ionicons size={ICON_SIZE} name={androidName} color="white" />
+        }
+      />
+    </Pressable>
+  );
+}
